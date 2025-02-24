72165df5b53924366f82b9d0aed05b764913691f
Sasikanth R <137884114+Sasikanth2001@users.noreply.github.com>
2025-02-24 19:31:23 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -21,7 +21,7 @@ export const Hero = () => {
       
           </div>
           <img
-              src="https://drive.google.com/uc?export=view&id=13mKwn8WrDwewDahSRVjbVCu6O4FT4O0V"
+              src='https://drive.google.com/uc?export=view&id=13mKwn8WrDwewDahSRVjbVCu6O4FT4O0V'
               alt="My image"
               className={styles.heroImg}
           />
